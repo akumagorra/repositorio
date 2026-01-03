0f8a6648b5383f5b872939b5705445955638132b
akumagorra <cecervantes2011@gmail.com>
2026-01-03 12:28:56 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+
+from settings import settings
 import discord
+# import * - es una forma rápida de importar todos los archivos de la biblioteca
+from bot_logic import *
 
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
@@ -7,19 +11,27 @@ intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
 client = discord.Client(intents=intents)
 
+
+# Una vez que el bot esté listo, ¡imprimirá su nombre!
 @client.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'We have logged in as {client.user}')
 
+
+# Cuando el bot reciba un mensaje, ¡enviará mensajes en el mismo canal!
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
+        await message.channel.send('¡Hola! Soy un bot')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
     else:
-        await message.channel.send(message.content)
+        await message.channel.send("No puedo procesar este comando, ¡lo siento!")
 
-client.run("¡EL TOKEN DE TU BOT DEBERÍA ESTAR AQUÍ!")
+client.run(settings["TOKEN"])
